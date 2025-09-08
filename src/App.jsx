d3f44278e79d970886cb5f99473022a9682db945
Hassan Javaid <javaidhassan464@gmail.com>
2025-09-08 19:02:38 +0500

    fix case issue in whyUs component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import { useEffect } from 'react'
-import gsap from 'gsap'
 //Components
 import Navbar from './components/Navbar/Navbar.jsx'
 import Home from "./components/Home/Home.jsx"
 import PrincipleMessage from './components/PrincipalMessage/PrincipleMessage.jsx'
-import WhyUS from './components/WhyUS/WhyUS.jsx'
+import WhyUS from './components/WhyUs/WhyUs.jsx'
 import InfiniteScrollingText from './components/InfiniteScrollingText/InfiniteScrollingText.jsx'
 import Campuses from './components/campuses/campuses.jsx'
 import ContactUs from './components/contactUs/contactUs.jsx'
